c9f42eeee903e060f971fa9ffd0dbdf7a51ab3d8
Yuriy Sogokon <ysogokon@msn.com>
2021-09-22 08:44:45 -0400

    Added prettierrc
    Signed-off-by: Yuriy Sogokon <ysogokon@msn.com>

--- migrations/3_deploy_sfst.js
+++ migrations/3_deploy_sfst.js
@@ -1,7 +1,7 @@
-const SunFlowerSeedToken = artifacts.require("SunFlowerSeedToken");
-const Tether = artifacts.require("Tether");
-const RWD = artifacts.require("RWD");
-const DecentralBank = artifacts.require("DecentralBank");
+const SunFlowerSeedToken = artifacts.require('SunFlowerSeedToken');
+const Tether = artifacts.require('Tether');
+const RWD = artifacts.require('RWD');
+const DecentralBank = artifacts.require('DecentralBank');
 
 module.exports = async function(deployer, network, accounts) {
   await deployer.deploy(SunFlowerSeedToken);
@@ -20,10 +20,10 @@ module.exports = async function(deployer, network, accounts) {
   const decentralBank = await DecentralBank.deployed();
 
   // Transfer all tokens to DecentralBank (1 million)
-  await rwd.transfer(decentralBank.address, "1000000000000000000000000");
+  await rwd.transfer(decentralBank.address, '1000000000000000000000000');
 
   // Transfer 100 Mock Tether tokens to investor
-  await tether.transfer(accounts[1], "100000000000000000000");
+  await tether.transfer(accounts[1], '100000000000000000000');
 
-  await seedToken.transfer(accounts[1], "100000000000000000000");
+  await seedToken.transfer(accounts[1], '100000000000000000000');
 };
--- test/decentralBank.test.js
+++ test/decentralBank.test.js
@@ -1,17 +1,17 @@
-const SunFlowerSeedToken = artifacts.require("SunFlowerSeedToken");
-const Tether = artifacts.require("Tether");
-const RWD = artifacts.require("RWD");
-const DecentralBank = artifacts.require("DecentralBank");
+const SunFlowerSeedToken = artifacts.require('SunFlowerSeedToken');
+const Tether = artifacts.require('Tether');
+const RWD = artifacts.require('RWD');
+const DecentralBank = artifacts.require('DecentralBank');
 
-require("chai")
-  .use(require("chai-as-promised"))
+require('chai')
+  .use(require('chai-as-promised'))
   .should();
 
-contract("DecentralBank", ([owner, customer]) => {
+contract('DecentralBank', ([owner, customer]) => {
   let tether, reward, decentralBank;
 
   function tokens(number) {
-    return web3.utils.toWei(number, "ether");
+    return web3.utils.toWei(number, 'ether');
   }
 
   before(async () => {
@@ -21,76 +21,76 @@ contract("DecentralBank", ([owner, customer]) => {
     seedToken = await SunFlowerSeedToken.new();
 
     // Transfer all tokens to DecentralBank (1 million)
-    await reward.transfer(decentralBank.address, tokens("1000000"));
+    await reward.transfer(decentralBank.address, tokens('1000000'));
 
     // Transfer 100 tokes Tethers to Customer
-    await tether.transfer(customer, tokens("100"), { from: owner });
+    await tether.transfer(customer, tokens('100'), { from: owner });
   });
 
-  describe("Tether Deployment", async () => {
-    it("matches name successfully", async () => {
+  describe('Tether Deployment', async () => {
+    it('matches name successfully', async () => {
       const name = await tether.name();
-      assert.equal(name, "Tether");
+      assert.equal(name, 'Tether');
     });
   });
 
-  describe("Reward Token Deployment", async () => {
-    it("matches name successfully", async () => {
+  describe('Reward Token Deployment', async () => {
+    it('matches name successfully', async () => {
       const name = await reward.name();
-      assert.equal(name, "Reward Token");
+      assert.equal(name, 'Reward Token');
     });
   });
 
-  describe("Decentral Bank Deployment", async () => {
-    it("matches name successfully", async () => {
+  describe('Decentral Bank Deployment', async () => {
+    it('matches name successfully', async () => {
       const name = await decentralBank.name();
-      assert.equal(name, "Decentral Bank");
+      assert.equal(name, 'Decentral Bank');
     });
 
-    it("contract has tokens", async () => {
+    it('contract has tokens', async () => {
       let balance = await reward.balanceOf(decentralBank.address);
-      assert.equal(balance, tokens("1000000"));
+      assert.equal(balance, tokens('1000000'));
     });
   });
 
-  describe("Yield farming", async () => {
-    it("rewards tokens for staking", async () => {
+  describe('Yield farming', async () => {
+    it('rewards tokens for staking', async () => {
       let result;
       // Check inverstor balance
       result = await tether.balanceOf(customer);
       assert.equal(
         result.toString(),
-        tokens("100"),
-        "customer wallet balance before staking"
+        tokens('100'),
+        'customer wallet balance before staking'
       );
       // Check stakig for customer
-      await tether.approve(decentralBank.address, tokens("100"), {
-        from: customer,
+      await tether.approve(decentralBank.address, tokens('100'), {
+        from: customer
       });
-      await decentralBank.depositTokens(tokens("100"), { from: customer });
+      await decentralBank.depositTokens(tokens('100'), { from: customer });
 
       // Check update customer's balance
       result = await tether.balanceOf(customer);
       assert.equal(
         result.toString(),
-        tokens("0"),
-        "customer wallet balance after staking"
+        tokens('0'),
+        'customer wallet balance after staking'
       );
 
       // Check updated balance of DecentralBank
       result = await tether.balanceOf(decentralBank.address);
       assert.equal(
         result.toString(),
-        tokens("100"),
-        "DecentralBank updated balance after staking from customer"
+        tokens('100'),
+        'DecentralBank updated balance after staking from customer'
       );
 
       // Is Staking balance
       result = await decentralBank.isStaking(customer);
       assert.equal(
         result.toString(),
-        "true",
-        "customer is staking status after staking"
+        'true',
+        'customer is staking status after staking'
       );
 
       // Issue Tokens
